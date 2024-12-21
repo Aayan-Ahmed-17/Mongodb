06df58813e92361a6f30428b5b3901f51c0ce5f6
Aayan Ahmed <aayanahmedtejani@gmail.com>
2024-12-21 13:38:01 +0500

    git done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv"
 import express from "express"
 import cors from "cors"
 import connectDB from "./src/db/index.js"
+import todoRoutes from "./src/routes/todos.routes.js"
 
 dotenv.config()
 const app = express()
@@ -10,6 +11,8 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+//routes
+app.use('/api/v1' , todoRoutes)
 
 app.get('/', (req, res) => {
     console.log(process.env.MONGO_URI);
